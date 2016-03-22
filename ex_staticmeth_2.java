25a3ab20e288dc0c576b4ef88226bc4ef68ea4d0
Fuxkuhrz <gh0stsync.rt@gmail.com>
2016-03-22 11:11:17 +0700

    Create ex_staticmeth_2.java

---
+++ ex_staticmeth_2.java
@@ -0,0 +1,10 @@
+class  ex_staticmeth_2{
+	public static void main(String[] args) {
+		sum(10,20);
+	}
+	//-------------------------------------------
+	public static void sum(int a, int b){
+		System.out.println("Result : " + (a + b));
+	}
+	//-------------------------------------------
+}
