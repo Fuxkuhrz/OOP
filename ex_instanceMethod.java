22863de695c4c0ebe452b742edfaf36f79394d3e
Fuxkuhrz <gh0stsync.rt@gmail.com>
2016-03-29 11:18:22 +0700

    Create ex_instanceMethod.java

---
+++ ex_instanceMethod.java
@@ -0,0 +1,10 @@
+class ex_instanceMethod {
+	public static void main(String[] args) {
+		ex_instanceMethod obj = new ex_instanceMethod();
+		obj.print();
+	}
+	//---------------------------------------------------
+	public void print() {
+		System.out.println("Hello World!");
+	}
+}
