52480aee23387914eeb8108eede3fce54da0f847
Fuxkuhrz <gh0stsync.rt@gmail.com>
2016-04-11 12:01:38 +0700

    Create ex_constru.java

---
+++ ex_constru.java
@@ -0,0 +1,15 @@
+class ex_constru {
+	public ex_constru(int arg){
+		System.out.println(arg);
+	}
+	public ex_constru(String arg){
+		System.out.println(arg);
+	}
+	public ex_constru(double arg){
+		System.out.println(arg);
+	}
+
+	public static void main(String[] args) {
+	ex_constru obj = new ex_constru("String");
+	}
+}
