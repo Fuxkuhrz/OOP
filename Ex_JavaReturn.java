5b15f6c41f39e5d8cd67b28855e999874c906de3
Fuxkuhrz <gh0stsync.rt@gmail.com>
2016-04-11 12:01:58 +0700

    Create Ex_JavaReturn.java

---
+++ Ex_JavaReturn.java
@@ -0,0 +1,13 @@
+class  Ex_JavaReturn{
+	public static void main(String[] args) {
+		Ex_JavaReturn obj = new Ex_JavaReturn();
+		int day = 4;
+		int mount = 12;
+		int year = 2538;
+		int result = BirthFinder(day,mount,year);
+		System.out.println("");
+	}
+	public int BirthFinder(int day, int mount,  int year){
+	
+	}
+}
